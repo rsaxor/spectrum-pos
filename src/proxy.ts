6fd6c335c7a639ae42dc5ac42c1096c667be074c
rsaxor <44944087+rsaxor@users.noreply.github.com>
2025-10-28 12:11:34 +0400

    migrate: middleware->proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("firebase-auth-token")?.value;
   const { pathname } = request.nextUrl;
   const isAuthPage = pathname.startsWith("/login");
